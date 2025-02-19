7d335a6ca48d9339acb3a0bcc63f6f35bb9f3173
Aryo Adi Putro <aryoadiputro@gmail.com>
2025-02-19 16:22:23 +0700

    pertemuan2.praktikum

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function hello()
+    {
+        return 'Hello World';
+    }
+
+    // public function greeting()
+    // {
+    //     return view('blog.hello', ['name' => 'Bambang']);
+    // }
+
+    public function greeting()
+    {
+        return view('blog.hello')
+            ->with('name', 'Bambang')
+            ->with('occupation', 'Kapal Laut');
+    }
+}
